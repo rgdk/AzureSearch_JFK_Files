c801608a24832fe8d56a00cfcc531ca5486d4a5e
rgdk <daniel_karp@hotmail.com>
2020-08-10 17:18:31 +1000

    logos

--- frontend/src/common/components/logo-jfk/logo.component.tsx
+++ frontend/src/common/components/logo-jfk/logo.component.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export const LogoJFKComponent = ({ classes }) => (
   <div className={classes.container}>
-    <img src="../../mstile-70x70.png" alt="The Whitlam Dismissal" />
+    <img src="../mstile-70x70.png" alt="The Whitlam Dismissal" />
   </div>
 );

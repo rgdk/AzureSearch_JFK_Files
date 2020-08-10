d60ee59dd8a3eac57f9b1451bf3d89066bf8167a
rgdk <daniel_karp@hotmail.com>
2020-08-10 15:50:06 +1000

    Update logo.component.tsx

--- frontend/src/common/components/logo-jfk/logo.component.tsx
+++ frontend/src/common/components/logo-jfk/logo.component.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export const LogoJFKComponent = ({ classes }) => (
   <div className={classes.container}>
-    <img src="../../../assets/favicon/android-chrome-512x512.png" alt="Logo" />
+    <img src="../../android-chrome-512x512.png" alt="Logo" />
   </div>
 );

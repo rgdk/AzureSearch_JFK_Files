29daeadf815e0c9b39796d9976bdaadfb2aae306
rgdk <daniel_karp@hotmail.com>
2020-08-10 16:01:29 +1000

    updated

--- frontend/src/common/components/logo-jfk/logo.component.tsx
+++ frontend/src/common/components/logo-jfk/logo.component.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export const LogoJFKComponent = ({ classes }) => (
   <div className={classes.container}>
-    <img src="../../android-chrome-512x512.png" alt="Logo" />
+    <img src="../../mstile-70x70.png" alt="The Whitlam Dismissal" />
   </div>
 );
